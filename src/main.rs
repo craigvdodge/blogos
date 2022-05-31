3ebf191ad9c13d23210d9ed68980d075a1f2703a
craigvdodge <craigvdodge@gmail.com>
2022-05-30 17:15:22 -0700

    Print msg via VGA text buffer. Boots in qemu with cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+// main.rs
 #![no_std]
 #![no_main]
 
@@ -10,7 +10,17 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello there.";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    
     loop {}
 }
